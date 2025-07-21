a01d6f435a8688fd54d0eaeef3290d3b59d84f37
idrees <idrees.patvekar15@gmail.com>
2025-07-21 17:09:37 +0530

    venv creted alogn with all files and folders

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mohd. Idrees",
+    author_email="idrees.patvekar15@gmail.com",
+    packages=find_packages()
+)
